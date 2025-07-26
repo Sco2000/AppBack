2fac0c01d55b4e28b92d5c92ef8e176e98766e8b
Sco2000 <ousmanemarra70@gmail.com>
2025-07-26 15:00:28 +0000

    add index

--- public/index.php
+++ public/index.php
@@ -2,3 +2,15 @@
 
 echo "Vous avez installé la dépendence de ScoO\n";
 echo "Merci et bonne utilisation";
+
+require_once '../vendor/autoload.php';
+require_once '../app/config/bootstrap.php';
+use App\core\App;
+
+// Charger les dépendances depuis le YAML
+App::loadDependenciesFromYaml(__DIR__.'/../app/config/dependencies.yaml');
+
+
+use App\core\Router;
+
+$routes = Router::resolveRoute($routes);
